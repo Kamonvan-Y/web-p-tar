924cfa24919dc10f570fdb16f22800c3226b8744
Kamonvan Yingwong <nun.kamonvan150101@hotmail.com>
2018-05-28 11:37:05 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
       
-        <header className="App-header">
-          <div className="cover">
-          
-          </div>
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      
+      <div id="hero">
+      <div id="hero-overlay"></div>
       </div>
     );
   }
