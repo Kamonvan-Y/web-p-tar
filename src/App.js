83b905f2476b3f1bfb4a14e62912217beb84ccc8
Kamonvan Yingwong <nun.kamonvan150101@hotmail.com>
2018-05-28 13:06:37 +0700

    -test

--- src/App.js
+++ src/App.js
@@ -6,11 +6,29 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <div>
+      <div class="wrapper">
+      <nav>
+          <div class="logo">LOGO</div>
+                        <div class="wrapper"></div>
+                        <ul>
+                              <li><a href="#">Home</a></li>
+                              <li><a href="#">About</a></li>
+                              <li><a href="#">Blog</a></li>
+                              <li><a href="#">Contact</a></li>
+                              <li><a class="active" href="#">log in</a></li>
+                        </ul>
+          
+      </nav>
       
+     <section class="sec 1"></section>
+     <section class="content">
+       <p>something</p>
+     </section>
       
-      <div id="hero">
-      <div id="hero-overlay"></div>
-      </div>
+
+</div>
+</div>
     );
   }
 }
